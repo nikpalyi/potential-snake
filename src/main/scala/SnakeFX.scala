78d0c44c2ad926e8c6284dada55dcd0db409d33b
nikpalyi <16964051+nikpalyi@users.noreply.github.com>
2022-03-29 17:28:56 +0100

    Modifying the content field of the scne to get a green drawing

--- src/main/scala/SnakeFX.scala
+++ src/main/scala/SnakeFX.scala
@@ -1,3 +1,31 @@
-object SnakeFX {
+// all the imports we're going to need in the entire app
+// (auto-import helps a lot, but adding them here in case of confusion)
+import scalafx.application.{JFXApp3, Platform}
+import scalafx.beans.property.{IntegerProperty, ObjectProperty}
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.paint.Color._
+import scalafx.scene.shape.Rectangle
 
+import scala.concurrent.Future
+import scala.util.Random
+
+object SnakeFx extends JFXApp3 {
+  override def start(): Unit = {
+    stage = new JFXApp3.PrimaryStage {
+      width = 600
+      height = 600
+      scene = new Scene {
+        fill = White
+        // added just now
+        content = new Rectangle {
+          x = 50
+          y = 75
+          width = 25
+          height = 25
+          fill = DarkGreen
+        }
+      }
+    }
+  }
 }
